1f4a6ae5cda92af880fc438e3c66cadcaeac7d85
Daniel Ghazvinie <danielghazvinie@protonmail.com>
2021-11-15 21:06:23 +0000

    adding insertion sort

--- arrays-sets.js
+++ arrays-sets.js
@@ -124,3 +124,23 @@ const selectionSort = (arr) => {
     };
     return arr;
 };
+
+const insertionSort = (arr) => {
+    for (let i = 1; i< arr.length; i++){
+        let temp = arr[i];
+        let position = i - 1;
+
+        while(position >= 0){
+            if (arr[position] > temp){
+                arr[position +1] = arr[position];
+                position = position - 1;
+            } else {
+                break;
+            }
+        }
+        arr[position + 1] = temp;
+    }
+    return arr;
+};
+
+console.log(insertionSort(myUnorderedArr));
